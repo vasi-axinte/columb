aa24abc5387203fbee5bca0f8c88926185115423
Ursu1977 <tomicsaa@yahoo.de>
2025-04-23 17:00:49 +0300

    Add files via upload

---
+++ src/app/static-data/l176-constants.ts
@@ -0,0 +1,212 @@
+import { LawDetails } from "../models/sanction";
+
+export const l176List: LawDetails[] = [
+    {
+        parentId: 8,
+        contentId: 1,
+        title: "1  Pescuitul recreativ fără permis",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+            _________________________________________________
+            
+            FAPTA COMISĂ
+            
+            
+            Art. 51
+
+            a) pescuitul recreativ de către persoane care nu dețin sau nu au asupra lor permisul de pescuit recreativ sau, după caz, elementele de identificare, în format letric sau electronic, din care să rezulte legalitatea acțiunii.
+            _________________________________________________
+            
+            PREVĂZUTĂ DE:   Art. 51, lit. a)
+            SANCȚIONATĂ DE: Art. 51
+            _________________________________________________
+            
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 1000 la 1500 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 500 lei.
+            _________________________________________________
+            
+            MĂSURI COMPLEMENTARE: NU SUNT
+        `,
+    },
+    {
+        parentId: 8,
+        contentId: 2,
+        title: "2  Refuzul prezentării permisului, licenței sau autorizatiei",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+
+            _________________________________________________
+
+            FAPTA COMISĂ
+
+            Art. 51
+
+            c) refuzul prezentării permisului, licenței sau autorizației, după caz, atunci când acestea sunt solicitate de către persoanele împuternicite;
+            _________________________________________________
+
+            PREVĂZUTĂ DE:   Art. 51, lit. c) 
+            SANCȚIONATĂ DE: Art. 51
+            _________________________________________________
+
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 1000 la 1500 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 500 lei.
+            _________________________________________________
+
+            MĂSURI COMPLEMENTARE: NU SUNT
+        `
+    },
+    {
+        parentId: 8,
+        contentId: 3,
+        title: "3 Pescuitul pe Dunare cu peste 3 undițe 4 lansete...",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+            _________________________________________________
+            
+            FAPTA COMISĂ
+            
+            Art. 51
+
+            e) exercitarea pescuitul recreativ în apele din zona colinară și de șes, pe tot cursul Dunării și pe brațele sale cu mai mult de 3 undițe și/sau 4 lansete cu câte două cârlige pe fiecare.
+            _________________________________________________
+            
+            PREVĂZUTĂ DE:   Art. 51, lit. e) 
+            SANCȚIONATĂ DE: Art. 51
+            _________________________________________________
+            
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 1000 la 1500 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 500 lei.
+            _________________________________________________
+            
+            MĂSURI COMPLEMENTARE: NU SUNT
+        `
+    },
+    {
+        parentId: 8,
+        contentId: 4,
+        title: "4   Transportul deșeurilor periculoase rezultate din activitatea medicală",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+            _________________________________________________
+            
+            FAPTA COMISĂ
+            
+            Art. 25
+            (2) Următoarele fapte constituie contravenții și se sancționează după cum urmează:
+            
+            a) ... nerespectarea prevederilor art. 24 alin. (2), alin. (3) lit. a), b) și d) §
+            
+            § Art. 24
+            alin. (2) Nu se vor accepta la transport deșeurile ale căror ambalaje nu sunt etichetate cu următoarele informații: datele de identificare ale unității sanitare, secția/laborator, cantitate și data umplerii.
+            
+            alin. (3) Transportul deșeurilor periculoase rezultate din activitatea medicală se face ținându-se cont de următoarele:
+            
+            a) toate vehiculele utilizate pentru transportul deșeurilor periculoase rezultate din activitatea medicală sunt evaluate de Ministerul Sănătății Publice, prin institutele de sănătate publică, pe baza unei metodologii ce va fi aprobată prin ordin al ministrului sănătății publice, în termen de 60 de zile de la intrarea în vigoare a prezentei hotărâri. În interiorul vehiculelor, deșeurile periculoase rezultate din activitatea medicală se transportă în containere din plastic cu capac, tip europubelă, în poziție verticală;
+            
+            b) vehiculul utilizat pentru transportul deșeurilor periculoase rezultate din activitatea medicală trebuie să îndeplinească următoarele cerințe :
+            (i) să fie destinat, amenajat special și utilizat numai pentru transportul deșeurilor periculoase rezultate din activitatea medicală;
+            (ii) să fie autoutilitară sau autovehicul cu masa totală maximă autorizată;
+            (iii) compartimentul destinat containerelor să fie separat de cabina șoferului, să fie realizat din materiale ușor lavabile și rezistente la abur și agenți chimici folosiți la curățenie și dezinfecție;
+            (iv) să aibă în dotare containere din plastic cu capac, tip europubelă, în care sunt depozitate deșeurile ambalate în timpul transportului, în condiții de siguranță;
+            (v) să aibă dispozitive de fixare a containerelor în timpul transportului;
+            (vi) să conțină sisteme de asigurare împotriva răspândirii deșeurilor periculoase în mediu în caz de accident;
+            (vii) să existe permanent o trusă sau un minicompartiment ce conține saci din plastic, echipament de protecție, echipamente de curățenie, dezinfecție, trusă de prim-ajutor;
+            (viii) să fie prevăzut cu trusă conform prevederilor Acordului european privind transportul internațional rutier al mărfurilor periculoase (A.D.R.);
+            (ix) compartimentul destinat containerelor va fi prevăzut cu sisteme de răcire a aerului: aer condiționat, echipament frigorific;
+            (x) autovehiculul va fi marcat și inscripționat cu datele de identificare ale societății transportatoare pe ambele părți laterale, activitatea prestată "Transport deșeuri periculoase rezultate din activitatea medicală", pictograma "Pericol biologic" și marcaj A.D.R.;
+            
+            d) transportul deșeurilor periculoase de la unitatea medicală în care acestea se produc și care este expeditorul deșeurilor și până la destinatarul deșeurilor, reprezentat de instalația în care acestea sunt tratate sau eliminate, se realizează cu respectarea prevederilor prezentei hotărâri.
+            
+            _________________________________________________
+            
+            PREVĂZUTĂ DE:   Art. 24, alin (2) și (3) lit. a), b) și d)
+            SANCȚIONATĂ DE: Art. 25, alin.(2), lit. a)
+            _________________________________________________
+            
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 15000 la 30000 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 7500 lei.
+            _________________________________________________
+            
+            MĂSURI COMPLEMENTARE:        
+            Art. 25
+            alin. (3) În cazul săvârșirii contravențiilor prevăzute la alin. (2) lit. a), se aplică pe lângă sancțiunea contravențională principală și sancțiunea contravențională complementară a confiscării mijlocului de transport destinat sau folosit la săvârșirea faptei.
+        `
+    },
+    {
+        parentId: 8,
+        contentId: 5,
+        title: "5  Transportul deșeurilor periculoase pe altă rută",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+            _________________________________________________
+            
+            FAPTA COMISĂ
+            
+            Art. 25
+            (2) Următoarele fapte constituie contravenții și se sancționează după cum urmează:
+            
+            c) nerespectarea prevederilor art. 9, alin. (2) §
+            
+            § Art. 9
+            alin. (2) Transportatorul transportă deșeurile periculoase pe ruta aprobată în formularul de aprobare a transportului deșeurilor periculoase și le predă destinatarului.
+            _________________________________________________
+            
+            PREVĂZUTĂ DE:   Art. 24, alin (2)
+            SANCȚIONATĂ DE: Art. 25, alin.(2), lit. c)
+            _________________________________________________
+            
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 5000 la 10000 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 2500 lei.
+            _________________________________________________
+            
+            MĂSURI COMPLEMENTARE: NU
+        `
+    },
+    {
+        parentId: 8,
+        contentId: 6,
+        title: "6 Abandonarea deșeurilor pe traseu",
+        text: `       
+            ACT NORMATIV
+
+            LEGEA nr. 176 din 3 iunie 2024
+            pescuitului și a protecției resursei acvatice vii
+            _________________________________________________
+            
+            FAPTA COMISĂ
+            
+            Art. 25
+            (2) Următoarele fapte constituie contravenții și se sancționează după cum urmează:
+            
+            a) abandonarea deșeurilor pe traseu de către operatorii de transport
+            _________________________________________________
+            
+            PREVĂZUTĂ DE:   Art. 25, alin.(2), lit. a)
+            SANCȚIONATĂ DE: Art. 25, alin.(2), lit. a)
+            _________________________________________________
+            
+            AMENDĂ CONTRAVENŢIONALĂ: amendă de la 15000 la 30000 lei.
+            JUMĂTATE DIN MINIM ÎN 15 ZILE : 7500 lei.
+            _________________________________________________
+            
+            MĂSURI COMPLEMENTARE:
+            Art. 25
+            alin. (3) În cazul săvârșirii contravențiilor prevăzute la alin. (2) lit. a), se aplică pe lângă sancțiunea contravențională principală și sancțiunea contravențională complementară a confiscării mijlocului de transport destinat sau folosit la săvârșirea faptei.
+        `
+    }
+]
